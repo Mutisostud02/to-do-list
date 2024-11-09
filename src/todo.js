a937a98a3961f61366012476850081a592f96111
Mutisostud02 <https://github.com/Mutisostud02>
2024-11-09 11:27:27 +0300

    added a todo.js file

---
+++ src/todo.js
@@ -0,0 +1,12 @@
+function ToDoList () {
+    const title = ""
+    const description = ""
+    const dueDate = "";
+    const priority = "";
+    const notes = "";
+    const checkList = "";
+    return {title, description, dueDate, priority, notes, checkList};
+}
+
+export const createToDo = ToDoList();
+const projects = ["default"];
