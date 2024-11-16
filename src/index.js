76a00cc728a323b43522c18ca85751971cb506b0
Mutisostud02 <https://github.com/Mutisostud02>
2024-11-16 22:51:52 +0300

    Show form when submit button is clicked
    done by toggling class "form" on the form element

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import "./styles.css";
 import { createToDo } from "./todo";
 console.log('Hello, we on')
 console.log(createToDo.priority)
+const doc = document.querySelector('form');
+
+const addBtn = document.querySelector('.addProj');
+addBtn.addEventListener('click', () => {
+    console.log('Clicked')
+    doc.classList.toggle('form');
+})
+console.log(addBtn)
