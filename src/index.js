1732efb033b4f069340cd8af2a2c8e2fbae89ddf
Mutisostud02 <https://github.com/Mutisostud02>
2024-11-18 22:26:54 +0300

    Added submit eventListener
    when form is submitted the answers are stored as an object and pushed to defaultPro array

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
-import { createToDo } from "./todo";
-console.log('Hello, we on')
-console.log(createToDo.priority)
+import { ToDoList } from "./todo";
+import { addToDo } from "./addTodo";
+import { defaultPro } from "./todo";
 const doc = document.querySelector('form');
 
 const addBtn = document.querySelector('.addProj');
@@ -9,4 +9,28 @@ addBtn.addEventListener('click', () => {
     console.log('Clicked')
     doc.classList.toggle('form');
 })
-console.log(addBtn)
+const title = document.querySelector('#title')
+const description = document.querySelector('#description')
+const dueDate = document.querySelector('#dueDate')
+const priority = document.getElementsByName('priority');
+const notes = document.querySelector('#notes')
+const submit = document.querySelector('#submit');
+submit.addEventListener('click', (e) => {
+    console.log('Submitted');
+    const createToDo = ToDoList();
+    createToDo.title = title.value;
+    createToDo.description = description.value;
+    createToDo.dueDate = dueDate.value;
+    createToDo.notes = notes.value;
+    addToDo(createToDo, defaultPro);
+    title.value = "";
+    description.value = "";
+    dueDate.value = "";
+    notes.value = "";
+    doc.classList.toggle('form');
+    e.preventDefault();
+  
+})
+
+console.log(defaultPro);
+
