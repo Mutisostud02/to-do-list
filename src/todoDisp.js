dea8816589b5843871a1b8073f22cb2a66c93432
Mutisostud02 <https://github.com/Mutisostud02>
2024-11-18 17:55:40 +0300

    Added a new js file to display our todo lists

---
+++ src/todoDisp.js
@@ -0,0 +1,4 @@
+const proTitle = document.createElement('h4');
+const proDescr = document.createElement('p');
+const proDueTime = document.createElement('p');
+const notes = document.createElement('p');
