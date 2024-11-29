ad07968ebffd45e3421d3ee1715ce50bb449aea4
Mutisostud02 <https://github.com/Mutisostud02>
2024-11-29 21:19:13 +0300

    Populated screen with our todo lists when the form is submitted
    Added logic to determine the radio priority selected. Created html elements to hold newly added todo list and their properties. Added classes to our todo list containers and appended them to the todoList class to be displayed.

--- src/index.js
+++ src/index.js
@@ -5,32 +5,65 @@ import { defaultPro } from "./todo";
 const doc = document.querySelector('form');
 
 const addBtn = document.querySelector('.addProj');
+
 addBtn.addEventListener('click', () => {
     console.log('Clicked')
     doc.classList.toggle('form');
 })
+
 const title = document.querySelector('#title')
-const description = document.querySelector('#description')
 const dueDate = document.querySelector('#dueDate')
-const priority = document.getElementsByName('priority');
-const notes = document.querySelector('#notes')
+const proPriority = document.querySelectorAll('input[name="priority"]');
+const priority = Array.from(proPriority);
 const submit = document.querySelector('#submit');
+
 submit.addEventListener('click', (e) => {
-    console.log('Submitted');
+    const todoList = document.querySelector('.toDoList');
+    let selectedPriority = null;
+    priority.forEach(element => {
+        if(element.checked) {
+            selectedPriority = element.value;
+        }
+    });
+
     const createToDo = ToDoList();
+
     createToDo.title = title.value;
-    createToDo.description = description.value;
     createToDo.dueDate = dueDate.value;
-    createToDo.notes = notes.value;
+    createToDo.priority = selectedPriority;
+
     addToDo(createToDo, defaultPro);
+
+    const todoTitle = document.createElement('h3');
+    todoTitle.textContent = `${createToDo.title}`;
+    todoTitle.setAttribute('style', 'font-weight:700; color: rgb(36,148,223')
+
+    const hr = document.createElement('hr');
+
+    const todoDue = document.createElement('p');
+    todoDue.textContent = `DUE : ${createToDo.dueDate}`;
+
+    const todoPriority = document.createElement('p');
+    todoPriority.textContent = `PRIORITY : ${createToDo.priority}`;
+
+    let toDo = document.createElement('div');     
+    toDo.classList.add('todoItems');
+    toDo.appendChild(todoTitle);
+    toDo.appendChild(hr);
+    toDo.appendChild(todoDue);
+    toDo.appendChild(todoPriority);
+    todoList.appendChild(toDo);
+
     title.value = "";
-    description.value = "";
     dueDate.value = "";
-    notes.value = "";
+
     doc.classList.toggle('form');
+
+    
     e.preventDefault();
-  
+    
 })
 
 console.log(defaultPro);
+
 
